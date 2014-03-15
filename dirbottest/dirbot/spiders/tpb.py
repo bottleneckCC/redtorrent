16dfc57f501c61361c75f83022c29c7837afdc64
bottleneckCC <bottleneck@concentriccollective.com>
2014-03-14 17:17:49 -0700

    Latest Commit
    scrappy crawl tab and mtt2.py work separate, linked by link test.json

---
+++ dirbottest/dirbot/spiders/tpb.py
@@ -0,0 +1,36 @@
+from scrapy.spider import Spider
+from scrapy.selector import HtmlXPathSelector
+
+from dirbot.items import Website
+
+
+class DmozSpider(Spider):
+    name = "tpb"
+    allowed_domains = ["thepiratebay.se"]
+    start_urls = [
+        "http://thepiratebay.se/search/red%20dawn/0/7/0",
+    ]
+
+    def parse(self, response):
+        """
+        The lines below is a spider contract. For more info see:
+        http://doc.scrapy.org/en/latest/topics/contracts.html
+
+        @url http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/
+        @scrapes name
+        """
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@id="main-content"]//td')        
+        items = []
+
+        for site in sites:
+            item = Website()
+            item['url'] = site.select('a/@href').extract()
+            items.append(item)
+	    #dict(item)
+
+	return items
+	#item2 = Website(items.pop(1))
+	#item3 = item2['url']
+	#s = item3[0]
+	#return s.encode('utf')
